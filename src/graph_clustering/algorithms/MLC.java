03beff3ad0c38935d72ed86cdd24843c0dee0dc9
Aleksandr Salo <saloasdev@gmail.com>
2014-11-28 12:00:28 -0600

    article

--- src/graph_clustering/algorithms/MLC.java
+++ src/graph_clustering/algorithms/MLC.java
@@ -13,18 +13,64 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class MLC {
+	private static final double PRECISION = 0.00001; 
+	private static final int POWER = 2;
+	private static final int INFLATION = 2;
 	private static ArrayList<String> inputStrings;
 	private static final String FILENAME = "test.txt";// "assignment4_data.txt";
 	private static final String RESULT_FILENAME = "MLC.txt";
 	private static ArrayList<HashSet<String>> final_clusters = new ArrayList<HashSet<String>>();
 	private static ArrayList<String> nodes;
-	private static double[][] table;
+	private static Double[][] table;
 	private static int N;
 
 	public static void main(String[] args) {
 		Instant startTime = Instant.now();
 
 		initTable();
+		printTable();
+		for (int i = 1; i < 30; i++){
+			expand();
+			printTable();
+			inflate();
+			printTable();
+		}
+		// ReportPrinter.printReport(final_clusters, RESULT_FILENAME);
+
+		Instant endTime = Instant.now();
+		System.out.println(Duration.between(startTime, endTime));
+	}
+	
+	static void expand(){
+		table = multiply(table, table);
+	}
+	
+	static void inflate(){
+		//power
+		for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                table[i][j] = Math.pow(table[i][j], INFLATION);
+            }
+        }
+
+		//normalize
+		Double[] sums = new Double[N];
+		for (int i = 0; i < N; i++) 
+			sums[i] = 0.0;
+		for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+            	sums[i] += table[j][i];
+            }
+        }
+		for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+            	table[j][i] = table[j][i] / sums[i];
+            }
+        }
+	}
+	
+	static void printTable(){
+		System.out.println();
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				System.out.print(table[i][j]);
@@ -32,12 +78,7 @@ public class MLC {
 			}
 			System.out.println();
 		}
-		// ReportPrinter.printReport(final_clusters, RESULT_FILENAME);
-
-		Instant endTime = Instant.now();
-		System.out.println(Duration.between(startTime, endTime));
 	}
-
 	private static void initTable() {
 		readFile();
 		HashSet<String> names = new HashSet<String>();
@@ -49,30 +90,30 @@ public class MLC {
 		}
 		nodes = new ArrayList<String>(names);
 		N = nodes.size();
-		table = new double[N][N];
+		table = new Double[N][N];
 		for (int i = 0; i < N; i++)
 			for (int j = 0; j < N; j++)
-				table[i][j] = 0;
+				table[i][j] = 0.0;
 		for (String s : inputStrings) {
 			sc = new Scanner(s);
 			String s1 = sc.next();
 			String s2 = sc.next();
 			int i1 = nodes.indexOf(s1);
 			int i2 = nodes.indexOf(s2);
-			table[i1][i2] = 1;
-			table[i2][i1] = 1;
+			table[i1][i2] = 1.0;
+			table[i2][i1] = 1.0;
 		}
 
 		// self loop
 		for (int i = 0; i < N; i++)
-			table[i][i] = 1;
+			table[i][i] = 1.0;
 
 		// normalize
 		int[] sums = new int[N];
 		for (int i = 0; i < N; i++) {
 			sums[i] = 0;
 			for (int j = 0; j < N; j++)
-				sums[i] += (int) table[i][j];
+				sums[i] += table[i][j];
 		}
 		for (int i = 0; i < N; i++)
 			for (int j = 0; j < N; j++)
@@ -91,4 +132,34 @@ public class MLC {
 			e.printStackTrace();
 		}
 	}
+	
+	private static Double[][] multiply(Double[][] A, Double[][] B) {
+
+        int aRows = A.length;
+        int aColumns = A[0].length;
+        int bRows = B.length;
+        int bColumns = B[0].length;
+
+        if (aColumns != bRows) {
+            throw new IllegalArgumentException("A:Rows: " + aColumns + " did not match B:Columns " + bRows + ".");
+        }
+
+        Double[][] C = new Double[aRows][bColumns];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                C[i][j] = 0.00000;
+            }
+        }
+
+        for (int i = 0; i < aRows; i++) { // aRow
+            for (int j = 0; j < bColumns; j++) { // bColumn
+                for (int k = 0; k < aColumns; k++) { // aColumn
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        return C;
+    }
+	
 }
